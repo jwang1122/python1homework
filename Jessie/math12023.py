a07302f48d190cba0d2aa9b190cfd8b223cbe531
jessie <peizhen.zhu@gmail.com>
2020-09-09 18:33:21 -0500

    my first coding homework

---
+++ Jessie/math12023.py
@@ -0,0 +1,20 @@
+print("01. 2020-20= ",(2020-20))
+print("I like all the problems")
+a =2020
+b =20
+c =a-b
+print("02. %d - %d = %d"%(a,b,c))
+a =494
+b =118
+c =a+b
+print("03. %d + %d = %d"%(a,b,c))
+a = 60
+b =5
+c =a/b
+o = '/'
+print("04. %d %s %d = %d"%(a,o,b,c))
+a =14
+b =50
+c =a*b
+o='*'
+print("05. %d %s %d = %d"%(a,o,b,c))
