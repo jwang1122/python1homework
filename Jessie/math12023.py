859c358300819bdc20e3058a0a562e0e26372190
jessie <peizhen.zhu@gmail.com>
2020-09-13 15:40:58 -0500

    my python math homework

--- Jessie/math12023.py
+++ Jessie/math12023.py
@@ -1,13 +1,18 @@
-print("01. 2020-20= ",(2020-20))
+"""
+This code is for solving math problems.
+"""
 print("I like all the problems")
 a =2020
 b =20
 c =a-b
-print("02. %d - %d = %d"%(a,b,c))
+print("01. %d - %d = %d"%(a,b,c))
 a =494
 b =118
 c =a+b
-print("03. %d + %d = %d"%(a,b,c))
+print("02. %d + %d = %d"%(a,b,c))
+a=1296
+b=(a // 10)%10
+print("03. The tens digit of %d is %d "%(a,b))
 a = 60
 b =5
 c =a/b
@@ -18,3 +23,71 @@ b =50
 c =a*b
 o='*'
 print("05. %d %s %d = %d"%(a,o,b,c))
+a=66
+b=4
+c=a%b
+o='/'
+print("06. The remainder of %d %s %d is %d"%(a,o,b,c))
+a =14
+b =3
+c =a*b
+print("07. The product of %d and %d is %d."%(a,b,c))
+a=8
+b=11
+c=a*b+a
+o='*'
+print("08. %d %s %d + %d = %d"%(a,o,b,a,c))
+a=53
+b=12
+c=28
+d=7
+e=a+b+c+d
+print("09. %d + %d + %d + %d = %d"%(a,b,c,d,e))
+a=201
+b=402
+c=804
+d=603
+e=a+b+c+d
+print("10. %d + %d + %d + %d = %d"%(a,b,c,d,e))
+a=1987
+print("11. %d rounded to the nearest ten is %d"%(a,round(a,-1)))
+a=17
+b=a*a
+o='*'
+print("12. %d %s %d = %d"%(a,o,a,b))
+a=578
+b=361
+c=27
+d=a-b-c
+print("13. %d - %d - %d = %d"%(a,b,c,d))
+a=11
+b=48
+c=a*b
+o='*'
+print("14. %d %s %d = %d"%(a,o,b,c))
+a=681
+b=292
+c=a-b
+print("15. %d - %d = %d"%(a,b,c))
+a=45
+b=18
+c=a*b
+o='*'
+print("16. %d %s %d = %d"%(a,o,b,c))
+DCLXXVI=676
+print("17. DCLXXVI in arabic numerals is %d"%(DCLXXVI))
+a=37
+b=41
+c=45
+d=a+b+c
+print("18. %d + %d + %d = %d"%(a,b,c,d))
+a=32
+b=28
+c=a*b
+o='*'
+print("19. %d %s %d = %d"%(a,o,b,c))
+a=398
+b=202
+c=a*b
+o='*'
+print("20. %d %s %d = %d"%(a,o,b,c))
